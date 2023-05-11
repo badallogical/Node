0c1b3d5b74598ba7d961d1fc3a7e62a370029c23
badallogical <badallearn@gmail.com>
2023-05-11 16:25:14 +0530

    creating server with http

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-var m = require("./md1")
-console.log(m.factorical(4))
-console.log("sum is " , m.add(2,4));
+// var m = require("./md1")
+// console.log(m.factorical(4))
+// console.log("sum is " , m.add(2,4));
+
+const http = require("http")
+const port = 8080
+
+const server = http.createServer( function(req, res){
+    res.write("Hello everyone in backend development")
+    res.end()
+})
+
+server.listen(port, function(error){
+    if( error ){
+        console.log("something went wrong", error)
+    }else{
+        console.log("Server is listening to port ", port)
+    }
+})
