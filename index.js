7c5535b615830fbb069098210aa649241d548f06
badallogical <badallearn@gmail.com>
2023-05-11 16:37:24 +0530

    added html file to write as response

--- index.js
+++ index.js
@@ -3,11 +3,21 @@
 // console.log("sum is " , m.add(2,4));
 
 const http = require("http")
-const port = 8080
+const port = 3000
+const fs = require("fs")
 
 const server = http.createServer( function(req, res){
-    res.write("Hello everyone in backend development")
-    res.end()
+    res.writeHead( 200, { 'Content-Type' : 'text/html'} )
+    fs.readFile('home.html', function(error, data ){
+        if( error ){
+            res.writeHead(404)
+            res.write("error ")
+        }else{
+            res.end(data)
+        }
+    })
+    // res.write("Hello everyone in backend development")
+    // res.end()
 })
 
 server.listen(port, function(error){
